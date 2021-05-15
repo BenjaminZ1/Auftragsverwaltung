3108013054d562efe0ae6aabb6028db9099c3b3f
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-05-15 21:00:27 +0200

    Integrated IDesignTimeDbContextFactory to Migrations. (all migrations in one place)

--- source/src/Auftragsverwaltung.Infrastructure/Common/BaseDbContext.cs
+++ source/src/Auftragsverwaltung.Infrastructure/Common/BaseDbContext.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Auftragsverwaltung.Infrastructure.Customer;
 using Microsoft.EntityFrameworkCore;
 
 namespace Auftragsverwaltung.Infrastructure.Common
 {
-    class BaseDbContext : DbContext
+    public class BaseDbContext : DbContext
     {
         public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options) { }
         protected BaseDbContext(DbContextOptions options) : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=.\\ZBW; Database=Auftragsverwaltung; Trusted_Connection=True");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=.\\ZBW; Database=Auftragsverwaltung; Trusted_Connection=True");
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
         }
     }
 }
--- source/src/Auftragsverwaltung.Infrastructure/Migrations/Migrations.cs
+++ source/src/Auftragsverwaltung.Infrastructure/Migrations/Migrations.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using Auftragsverwaltung.Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Auftragsverwaltung.Infrastructure.Migrations
 {
-    class Migrations
+    public class Migrations
     {
+        public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<BaseDbContext>
+        {
+            public BaseDbContext CreateDbContext(string[] args)
+            {
+                //var configuration = new ConfigurationBuilder()
+                //    .SetBasePath(Directory.GetCurrentDirectory())
+                //    .AddJsonFile("appsettings.json", true)
+                //    .AddEnvironmentVariables()
+                //    .Build();
+
+                var builder = new DbContextOptionsBuilder<BaseDbContext>();
+
+                var connectionString = "Data Source=.\\ZBW; Database=Auftragsverwaltung; Trusted_Connection=True";
+
+                builder.UseSqlServer(connectionString,
+                    x => x.MigrationsAssembly(typeof(ApplicationDbContextFactory).Assembly.FullName));
+
+
+                return new BaseDbContext(builder.Options);
+            }
+        }
     }
 }
