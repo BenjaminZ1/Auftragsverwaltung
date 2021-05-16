2fe054f4e22d363fa6e1fef167dcef1450b0a405
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-05-16 13:47:10 +0200

    Added CustomerRepository

--- source/src/Auftragsverwaltung.Infrastructure/Common/AppDbContextFactory.cs
+++ source/src/Auftragsverwaltung.Infrastructure/Common/AppDbContextFactory.cs
@@ -6,9 +6,9 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace Auftragsverwaltung.Infrastructure.Common
 {
-    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
-        public AppDbContext CreateDbContext(string[] args)
+        public AppDbContext CreateDbContext(string[] args = null)
         {
             //var configuration = new ConfigurationBuilder()
             //    .SetBasePath(Directory.GetCurrentDirectory())
@@ -20,7 +20,7 @@ namespace Auftragsverwaltung.Infrastructure.Common
             var connectionString = "Data Source=.\\ZBW; Database=Auftragsverwaltung; Trusted_Connection=True";
 
             builder.UseSqlServer(connectionString,
-                x => x.MigrationsAssembly(typeof(ApplicationDbContextFactory).Assembly.FullName));
+                x => x.MigrationsAssembly(typeof(AppDbContextFactory).Assembly.FullName));
 
             return new AppDbContext(builder.Options);
         }
---
+++ source/src/Auftragsverwaltung.Infrastructure/Customer/CustomerRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Auftragsverwaltung.Domain.Common;
+using Auftragsverwaltung.Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Auftragsverwaltung.Infrastructure.Customer
+{
+    public class CustomerRepository : IAppRepository<Domain.Customer>
+    {
+        private readonly AppDbContextFactory _dbContextFactory;
+
+        public CustomerRepository(AppDbContextFactory dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<Domain.Customer> Get(int id)
+        {
+            await using AppDbContext context = _dbContextFactory.CreateDbContext();
+            Domain.Customer entity = await context.Customers.FirstOrDefaultAsync(e => e.CustomerId == id);
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<Domain.Customer>> GetAll()
+        {
+            await using AppDbContext context = _dbContextFactory.CreateDbContext();
+            List<Domain.Customer> entities = await context.Customers.ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<Domain.Customer> Create(Domain.Customer entity)
+        {
+            await using AppDbContext context = _dbContextFactory.CreateDbContext();
+            EntityEntry<Domain.Customer> createdEntity = await context.Customers.AddAsync(entity);
+            await context.SaveChangesAsync();
+
+            return createdEntity.Entity;
+        }
+
+        public async Task<Domain.Customer> Update(int id, Domain.Customer entity)
+        {
+            await using AppDbContext context = _dbContextFactory.CreateDbContext();
+            entity.CustomerId = id;
+            context.Customers.Update(entity);
+            await context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            await using AppDbContext context = _dbContextFactory.CreateDbContext();
+            Domain.Customer entity = await context.Customers.FirstOrDefaultAsync(e => e.CustomerId == id);
+            context.Customers.Remove(entity);
+            await context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
