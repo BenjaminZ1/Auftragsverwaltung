d4780db2954dc1185afa6929c86126dd7d3c3182
Riklef Irps <riklef.irps@gmail.com>
2021-05-23 16:23:02 +0200

    created OrderRepository

---
+++ source/src/Auftragsverwaltung.Infrastructure/Order/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using Auftragsverwaltung.Domain.Common;
+using Auftragsverwaltung.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auftragsverwaltung.Infrastructure.Order
+{
+    class OrderRepository : IAppRepository<Domain.Order>
+    {
+
+        private readonly AppDbContext _db;
+
+        public Task<Domain.Order> Create(Domain.Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Order> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Domain.Order>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Order> Update(int id, Domain.Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
