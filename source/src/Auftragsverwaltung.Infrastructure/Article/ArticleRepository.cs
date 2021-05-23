0af684fec251f5c7b0939069e7770a2c0aaddf29
Riklef Irps <riklef.irps@gmail.com>
2021-05-23 13:51:25 +0200

    implemented Get, GetAll and Update methods for ArticleRepository

--- source/src/Auftragsverwaltung.Infrastructure/Article/ArticleRepository.cs
+++ source/src/Auftragsverwaltung.Infrastructure/Article/ArticleRepository.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Auftragsverwaltung.Domain.Common;
+using Auftragsverwaltung.Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Auftragsverwaltung.Infrastructure.Article
 {
-    class ArticleRepository
+    class ArticleRepository : IAppRepository<Domain.Article>
     {
+
+        private readonly AppDbContext _db;
+
+        public Task<Domain.Article> Create(Domain.Article entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Domain.Article> Get(int id)
+        {
+            Domain.Article entity = await _db.Articles.FirstOrDefaultAsync(e => e.ArticleId == id);
+            return entity;
+        }
+
+        public async Task<IEnumerable<Domain.Article>> GetAll()
+        {
+            List<Domain.Article> entities = await _db.Articles.ToListAsync();
+            return entities;
+        }
+
+        public async Task<Domain.Article> Update(int id, Domain.Article entity)
+        {
+            entity.ArticleId = id;
+            _db.Articles.Update(entity);
+            await _db.SaveChangesAsync();
+
+            return entity;
+        }
     }
 }
