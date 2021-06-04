2cee280fd1cf57df134afd6b893459ec0ca0b21d
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-06-04 18:38:07 +0200

    implemented CustomerService functions

--- source/src/Auftragsverwaltung.Application/Dtos/CustomerDto.cs
+++ source/src/Auftragsverwaltung.Application/Dtos/CustomerDto.cs
@@ -1,6 +1,51 @@
-﻿namespace Auftragsverwaltung.Application.Dtos
+﻿using System;
+using System.Collections.Generic;
+using Auftragsverwaltung.Domain.Address;
+using Auftragsverwaltung.Domain.Common;
+using Auftragsverwaltung.Domain.Customer;
+using Auftragsverwaltung.Domain.Order;
+
+namespace Auftragsverwaltung.Application.Dtos
 {
     public class CustomerDto : AppDto
     {
+        public int CustomerId { get; set; }
+        public string Firstname { get; set; } = default!;
+        public string Lastname { get; set; } = default!;
+        public int AddressId { get; set; }
+        public  Domain.Address.Address Address { get; set; }
+        public string Email { get; set; } = default!;
+        public string Website { get; set; }
+        public Byte[] Password { get; set; } = default!;
+        public  ICollection<Domain.Order.Order> Orders { get; set; }
+        public ResponseDto<Customer> Response { get; set; }
+
+        public CustomerDto(Customer customer)
+        {
+            CustomerId = customer.CustomerId;
+            Firstname = customer.Firstname;
+            Lastname = customer.Lastname;
+            AddressId = customer.AddressId;
+            Address = customer.Address;
+            Email = customer.Email;
+            Website = customer.Website;
+            Password = customer.Password;
+            Orders = customer.Orders;
+        }
+
+        public CustomerDto(ResponseDto<Customer> response)
+        {
+            Response = response;
+            CustomerId = response.Entity.CustomerId;
+            Firstname = response.Entity.Firstname;
+            Lastname = response.Entity.Lastname;
+            AddressId = response.Entity.AddressId;
+            Address = response.Entity.Address;
+            Email = response.Entity.Email;
+            Website = response.Entity.Website;
+            Password = response.Entity.Password;
+            Orders = response.Entity.Orders;
+            response.Entity = null;
+        }
     }
 }
--- source/src/Auftragsverwaltung.Application/Service/CustomerService.cs
+++ source/src/Auftragsverwaltung.Application/Service/CustomerService.cs
@@ -1,38 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Auftragsverwaltung.Application.Common;
 using Auftragsverwaltung.Application.Dtos;
+using Auftragsverwaltung.Domain.Common;
 using Auftragsverwaltung.Domain.Customer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Auftragsverwaltung.Application.Service
 {
     class CustomerService : IAppService<CustomerDto, Customer>
     {
+        private readonly IAppRepository<Customer> _repository;
+        public CustomerService(IAppRepository<Customer> repository)
+        {
+            _repository = repository;
+        }
         public async Task<CustomerDto> Get(int id)
         {
-            throw new NotImplementedException();
+            var data = await _repository.Get(id);
+            var mappedData = new CustomerDto(data);
+            return mappedData;
         }
 
         public async Task<IEnumerable<CustomerDto>> GetAll()
         {
-            throw new NotImplementedException();
+            var data = await _repository.GetAll();
+            var mappedData = data.Select(x => new CustomerDto(x));
+            return mappedData;
         }
 
         public async Task<CustomerDto> Create(Customer entity)
         {
-            throw new NotImplementedException();
+            var response = await _repository.Create(entity);
+            var mappedResponse = new CustomerDto(response);
+            return mappedResponse;
         }
 
         public async Task<CustomerDto> Update(int id, Customer entity)
         {
-            throw new NotImplementedException();
+            var response = await _repository.Update(id, entity);
+            var mappedResponse = new CustomerDto(response);
+            return mappedResponse;
         }
 
         public async Task<CustomerDto> Delete(int id)
         {
-            throw new NotImplementedException();
+            var response = await _repository.Delete(id);
+            var mappedResponse = new CustomerDto(response);
+            return mappedResponse;
         }
     }
 }
