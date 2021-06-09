68da8753f2e09f2b66bb35298ce114d37dbaa3e5
Christian <christian.stoll94@gmail.com>
2021-06-09 23:53:46 +0200

    Create GetCustomerCommand

---
+++ source/src/Auftragsverwaltung.WPF/Commands/GetCustomerCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Auftragsverwaltung.Application.Common;
+using Auftragsverwaltung.Application.Dtos;
+using Auftragsverwaltung.Domain.Customer;
+using Auftragsverwaltung.WPF.ViewModels;
+using Auftragsverwaltung.WPF.Views;
+
+namespace Auftragsverwaltung.WPF.Commands
+{
+    public class GetCustomerCommand : AsyncCommandBase
+    {
+        private CustomerViewModel _customerViewModel;
+
+        private IAppService<CustomerDto, Customer> _customerService;
+
+        public GetCustomerCommand(CustomerViewModel customerViewModel, IAppService<CustomerDto, Customer> customerService)
+        {
+            _customerViewModel = customerViewModel;
+            _customerService = customerService;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            throw new NotImplementedException();
+            //await _customerService.Get(2);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- source/src/Auftragsverwaltung.WPF/ViewModels/CustomerViewModel.cs
+++ source/src/Auftragsverwaltung.WPF/ViewModels/CustomerViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Auftragsverwaltung.Application.Common;
+using Auftragsverwaltung.Application.Dtos;
+using Auftragsverwaltung.Application.Service;
+using Auftragsverwaltung.Domain.Customer;
+using Auftragsverwaltung.WPF.Commands;
 
 namespace Auftragsverwaltung.WPF.ViewModels
 {
     public class CustomerViewModel : ViewModelBase
     {
+        public GetCustomerCommand GetCustomerCommand { get; set; }
+
+        
+
+
+        public CustomerViewModel()
+        {
+            this.GetCustomerCommand = new GetCustomerCommand(this, null);
+        }
+
     }
 }
