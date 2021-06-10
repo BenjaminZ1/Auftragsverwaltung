4d7216c32b9318b11fd45c4c958537475ff61788
Riklef Irps <riklef.irps@gmail.com>
2021-06-10 20:12:14 +0200

    added ArticleGroupRepositoryTests

---
+++ source/tests/Auftragsverwaltung.Tests/ArticleGroupRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using Auftragsverwaltung.Domain.ArticleGroup;
+using Auftragsverwaltung.Domain.Common;
+using Auftragsverwaltung.Infrastructure.ArticleGroup;
+using Auftragsverwaltung.Infrastructure.Common;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auftragsverwaltung.Tests
+{
+    [TestFixture]
+    public class ArticleGroupRepositoryTests
+    {
+        private DbContextOptions<AppDbContext> _options;
+
+        [OneTimeSetUp]
+        public void BuildDbContext()
+        {
+            _options = InstanceHelper.AppDbContext_BuildDbContext();
+        }
+
+        [SetUp]
+        public void ResetDb()
+        {
+            InstanceHelper.ResetDb(_options);
+        }
+
+        [Test]
+        public async Task Create_WhenNew_ReturnsCorrectResult()
+        {
+            //arrange
+            var articleGroup = new ArticleGroup()
+            {
+                Name = "TestArticleGroupName"
+            };
+            var dbContextFactoryFake = A.Fake<AppDbContextFactory>();
+            A.CallTo(() => dbContextFactoryFake.CreateDbContext(null)).Returns(new AppDbContext(_options));
+            var articleGroupRepository = new ArticleGroupRepository(dbContextFactoryFake);
+
+            //act
+            var result = await articleGroupRepository.Create(articleGroup);
+
+            //assert
+            result.Should().BeOfType(typeof(ResponseDto<ArticleGroup>));
+            result.Flag.Should().BeTrue();
+        }
+
+    }
+}
