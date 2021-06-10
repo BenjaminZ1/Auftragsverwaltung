58d5f0faf17f90b39518bc6b1ebdd263e88f0fe5
Riklef Irps <riklef.irps@gmail.com>
2021-06-10 20:37:36 +0200

    added tests

--- source/tests/Auftragsverwaltung.Tests/ArticleGroupRepositoryTests.cs
+++ source/tests/Auftragsverwaltung.Tests/ArticleGroupRepositoryTests.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,5 +51,84 @@ namespace Auftragsverwaltung.Tests
             result.Flag.Should().BeTrue();
         }
 
+        [Test]
+        public async Task Get_WhenOk_ReturnsCorrectResult()
+        {
+            //arrange
+            await InstanceHelper.AddDbTestArticleGroup(_options);
+            int articleGroupId = 1;
+
+            var dbContextFactoryFake = A.Fake<AppDbContextFactory>();
+            A.CallTo(() => dbContextFactoryFake.CreateDbContext(null)).Returns(new AppDbContext(_options));
+            var articleGroupRepository = new ArticleGroupRepository(dbContextFactoryFake);
+
+            //act
+            var result = await articleGroupRepository.Get(articleGroupId);
+
+            //assert
+            result.Should().BeOfType(typeof(ArticleGroup));
+            result.ArticleGroupId.Should().Be(articleGroupId);
+        }
+
+        [Test]
+        public async Task GetAll_WhenOk_ReturnsCorrectResult()
+        {
+            //arrange
+            await InstanceHelper.AddDbTestArticleGroup(_options);
+
+            var dbContextFactoryFake = A.Fake<AppDbContextFactory>();
+            A.CallTo(() => dbContextFactoryFake.CreateDbContext(null)).Returns(new AppDbContext(_options));
+            var articleGroupRepository = new ArticleGroupRepository(dbContextFactoryFake);
+
+            //act
+            var result = await articleGroupRepository.GetAll();
+
+            //assert
+            var resultList = result.ToList();
+            resultList.Should().BeOfType(typeof(List<ArticleGroup>));
+            resultList.Count().Should().Be(1);
+        }
+
+        [Test]
+        public async Task Delete_WhenOk_ReturnsCorrectResult()
+        {
+            //arrange
+            await InstanceHelper.AddDbTestArticleGroup(_options);
+            int articleGroupId = 1;
+
+            var dbContextFactoryFake = A.Fake<AppDbContextFactory>();
+            A.CallTo(() => dbContextFactoryFake.CreateDbContext(null)).Returns(new AppDbContext(_options));
+            var articleGroupRepository = new ArticleGroupRepository(dbContextFactoryFake);
+
+            //act
+            var result = await articleGroupRepository.Delete(articleGroupId);
+
+            //assert
+            result.Flag.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Update_WhenOk_ReturnsCorrectResult()
+        {
+            //arrange
+            await InstanceHelper.AddDbTestArticleGroup(_options);
+            int articleGroupId = 1;
+            string newArticleGroupName = "NewArticleGroupName";
+
+            var dbContextFactoryFake = A.Fake<AppDbContextFactory>();
+            A.CallTo(() => dbContextFactoryFake.CreateDbContext(null)).Returns(new AppDbContext(_options));
+            var articleGroupRepository = new ArticleGroupRepository(dbContextFactoryFake);
+
+            var entity = articleGroupRepository.Get(articleGroupId);
+            var articleGroup = entity.Result;
+            articleGroup.Name = newArticleGroupName;
+
+            //act
+            var result = await articleGroupRepository.Update(articleGroupId, articleGroup);
+
+            //assert
+            result.Entity.Name.Should().BeEquivalentTo(newArticleGroupName);
+            result.Flag.Should().BeTrue();
+        }
     }
 }
