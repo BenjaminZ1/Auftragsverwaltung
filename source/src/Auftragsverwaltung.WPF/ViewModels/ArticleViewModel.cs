19b3a4515a233fa841b54839d459b122b8b23aa1
Riklef Irps <riklef.irps@gmail.com>
2021-06-27 15:15:59 +0200

    edited ArticleView

--- source/src/Auftragsverwaltung.WPF/ViewModels/ArticleViewModel.cs
+++ source/src/Auftragsverwaltung.WPF/ViewModels/ArticleViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Auftragsverwaltung.Application.Common;
+using Auftragsverwaltung.Application.Dtos;
+using Auftragsverwaltung.Application.Service;
+using Auftragsverwaltung.Domain.Article;
+using Auftragsverwaltung.WPF.Commands;
+using Auftragsverwaltung.WPF.State;
+
 
 namespace Auftragsverwaltung.WPF.ViewModels
 {
     public class ArticleViewModel : ViewModelBase
     {
+       
     }
 }
