ccba522729bf98a9d1dd8842d8ff06988e54a8cc
Riklef Irps <riklef.irps@gmail.com>
2021-06-27 15:40:30 +0200

    created ArticleService

---
+++ source/src/Auftragsverwaltung.Application/Service/ArticleService.cs
@@ -0,0 +1,56 @@
+﻿using Auftragsverwaltung.Application.Dtos;
+using Auftragsverwaltung.Domain.Article;
+using Auftragsverwaltung.Domain.Common;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auftragsverwaltung.Application.Service
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IAppRepository<Article> _repository;
+        public ArticleService(IAppRepository<Article> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ArticleDto> Create(Article entity)
+        {
+            var response = await _repository.Create(entity);
+            var mappedResponse = new ArticleDto(response);
+            return mappedResponse;
+        }
+
+        public async Task<ArticleDto> Delete(int id)
+        {
+            var response = await _repository.Delete(id);
+            var mappedResponse = new ArticleDto(response);
+            return mappedResponse; 
+        }
+
+        public async Task<ArticleDto> Get(int id)
+        {
+            var data = await _repository.Get(id);
+            var mappedData = new ArticleDto(data);
+            return mappedData;
+        }
+
+        public async Task<IEnumerable<ArticleDto>> GetAll()
+        {
+            var data = await _repository.GetAll();
+            var mappedData = data.Select(x => new ArticleDto(x));
+            return mappedData;
+        }
+
+        public async Task<ArticleDto> Update(int id, Article entity)
+        {
+            var response = await _repository.Update(id, entity);
+            var mappedResponse = new ArticleDto(response);
+            return mappedResponse;
+        }
+    }
+}
