8684c584424c1542df477d450b383353aa0c85bb
Riklef Irps <riklef.irps@gmail.com>
2021-07-06 01:54:37 +0200

    created IOrderService

---
+++ source/src/Auftragsverwaltung.Application/Service/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using Auftragsverwaltung.Application.Common;
+using Auftragsverwaltung.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auftragsverwaltung.Application.Service
+{
+    public interface IOrderService : IAppService<OrderDto>
+    {
+    }
+}
