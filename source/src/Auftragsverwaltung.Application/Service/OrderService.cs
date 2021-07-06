d38fb35a820096c5bf9020d225e408ad6c6addef
Riklef Irps <riklef.irps@gmail.com>
2021-07-06 02:32:14 +0200

    implemented IOrderService and edited Create method

--- source/src/Auftragsverwaltung.Application/Service/OrderService.cs
+++ source/src/Auftragsverwaltung.Application/Service/OrderService.cs
@@ -1,10 +1,58 @@
-﻿using System;
+﻿using Auftragsverwaltung.Application.Dtos;
+using Auftragsverwaltung.Domain.Common;
+using Auftragsverwaltung.Domain.Customer;
+using Auftragsverwaltung.Domain.Order;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Auftragsverwaltung.Application.Service
 {
-    class OrderService
+    public class OrderService : IOrderService
     {
+        private readonly IAppRepository<Order> _repository;
+        public OrderService(IAppRepository<Order> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<OrderDto> Create(OrderDto dto)
+        {
+            var entity = ConvertToEntity(dto);
+
+            var response = await _repository.Create(entity);
+            var mappedResponse = new OrderDto(response);
+            return mappedResponse;
+        }
+
+        internal Order ConvertToEntity(OrderDto orderDto)
+        {
+            return new Order()
+            {
+                Date = orderDto.Date,
+                Customer = orderDto.Customer,
+                Positions = orderDto.Positions
+            };
+        }
+
+        public Task<OrderDto> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDto> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OrderDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDto> Update(OrderDto entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
