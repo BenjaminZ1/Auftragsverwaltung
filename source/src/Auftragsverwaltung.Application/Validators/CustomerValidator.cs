c5050f4bdd35196dc4dc3acd14efbb5fd95f6ebd
Christian <christian.stoll94@gmail.com>
2021-08-05 20:35:04 +0200

    added validations

--- source/src/Auftragsverwaltung.Application/Validators/CustomerValidator.cs
+++ source/src/Auftragsverwaltung.Application/Validators/CustomerValidator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Auftragsverwaltung.Application.Dtos;
 using FluentValidation;
 
@@ -11,16 +8,14 @@ namespace Auftragsverwaltung.Application.Validators
     {
         public CustomerValidator()
         {
-            RuleFor(p => p.Email).NotEmpty();
-            RuleFor(x => x.Email).Must(IsEmailValid).WithMessage("Email ungültig");
+            RuleFor(x => x.Email).Must(x => x != null && IsEmailValid(x)).WithMessage("Email ungültig");
+            RuleFor(x => x.Website).Must(x => x != null && IsWebsiteValid(x)).WithMessage("Website ungültig");
+            //RuleFor(x => x.Password).Must(x => x != null && IsPasswordValid(x)).WithMessage("Passwort ungültig");
         }
-
-        //Implementieren Sie eine Validierung für Email-Adressen. Wenn Ihre Validierung nicht alle Fälle gemäss RFC 5322 abdeckt (weil z.B. das RegEx-Pattern für die letzten 1% Spezialfälle zu kompliziert würde) dokumentieren Sie, welche Fälle nicht abgedeckt sind.
-
-        //from https://emailregex.com/
 
         private bool IsEmailValid(string email)
         {
+            //deckt 99.9% ab
             Regex regex = new Regex(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
             Match match = regex.Match(email);
             if (match.Success)
@@ -28,29 +23,32 @@ namespace Auftragsverwaltung.Application.Validators
             return false;
         }
 
+        private bool IsCustomerNumberValid(string customerNumber)
+        {
+            Regex regex = new Regex(@"^CU[0-9]{6}");
+            Match match = regex.Match(customerNumber);
+            if (match.Success)
+                return true;
+            return false;
+        }
 
+        private bool IsWebsiteValid(string website)
+        {
+            Regex regex = new Regex(@"^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*([/][a-zA-Z0-9\-\.\=&]*)*$");
+            Match match = regex.Match(website);
+            if (match.Success)
+                return true;
+            return false;
+        }
 
-
-        //wenn create: Check Kundennummer
-        //Die Adressnummer muss zwingend mit dem Präfix «CU» beginnen (CU=Customer). Anschliessend soll eine 5stellige Nummer folgen.
-
-
-
-        //Check Website
-        //Die Validierung für die Eingabe der Website soll folgende Formate zulassen:
-        //www.google.com
-        //http://www.google.com
-        //https://www.google.com
-        //google.com
-        //Wahlweise soll eine Subdomain angegeben werden können(gilt für alle vier Formate) –
-        //bspw.: https://policies.google.com
-        //Die Adresse soll auch beliebig mit einem Pfad sowie Parameter ergänzt werden können(dies auch wieder für alle Formate) – bspw.: https://policies.google.com/technologies/voice?hl=de&gl=ch
-
-
-
-        //Check Passwort
-        //8 Zeichen, Zweingend einen Gross und einen Kleinbuchstaben, Zweingend eine Zahl
-
-
+        private bool IsPasswordValid(string password)
+        {
+            //passwort ist byte[]....
+            Regex regex = new Regex(@"@^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+            Match match = regex.Match(password);
+            if (match.Success)
+                return true;
+            return false;
+        }
     }
 }
