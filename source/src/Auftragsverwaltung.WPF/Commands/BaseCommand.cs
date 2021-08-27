7ee73dc9d1316fa5327ebdb7f89a0b4ff601a3cb
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-08-27 21:44:59 +0200

    removed unused boolean

--- source/src/Auftragsverwaltung.WPF/Commands/BaseCommand.cs
+++ source/src/Auftragsverwaltung.WPF/Commands/BaseCommand.cs
@@ -25,7 +25,7 @@ namespace Auftragsverwaltung.WPF.Commands
 
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null ? true : _canExecute(parameter);
+            return _canExecute == null || _canExecute(parameter);
         }
 
         public void Execute(object parameter)
