8b1a20438d8c27b5c0dc08727d16e5cbadde3faf
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-09-06 20:50:31 +0200

    changed datetime format on OrderList GridView

---
+++ source/src/Auftragsverwaltung.WPF/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auftragsverwaltung.WPF.Constants
+{
+    public static class Constants
+    {
+        public const string DateTimeUiFormat = "dd.MM.yyyy HH:mm:ss";
+    }
+}
