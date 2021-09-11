5054d343fbcff1255106daf0ceefc3175406e794
Benjamin Zech <benjamin.zech@ari-ag.ch>
2021-09-11 18:52:55 +0200

    added unittest for AppDbContextFactory

---
+++ source/tests/Auftragsverwaltung.Tests/AppDbContextFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Auftragsverwaltung.Infrastructure.Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Auftragsverwaltung.Tests
+{
+    [TestFixture]
+    class AppDbContextFactoryTests
+    {
+        [Test]
+        public void CreateDbContext_WhenOk_ReturnsCorrectResult()
+        {
+            //arrange
+            var appDbContextFactory = new AppDbContextFactory();
+
+            //act
+            var result = appDbContextFactory.CreateDbContext(new []{"test"});
+
+            //assert
+            result.Should().BeOfType(typeof(AppDbContext));
+        }
+    }
+}
